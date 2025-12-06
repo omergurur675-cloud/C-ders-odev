7ca62b6563ecb0cf114c96119651d0321d00cdfa
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:57:13 +0300

    Update soru4.c

--- CONTINUE/soru4.c
+++ CONTINUE/soru4.c
@@ -1,3 +1,16 @@
+/*
+SORU 4 : Bu program, 5 tane girilen not içinden sadece 50 ve üzeri olanları ekrana yazdırır.
+
+SORU ALGORİTMASI:
+1- Başla
+2- 5 kez tekrar et
+3-     notu oku
+4-     Eğer not < 50 ise devam et (atla)
+5-     Değilse notu yazdır
+6- Bitir
+
+*/
+
 #include <stdio.h>
 
 int main() {
