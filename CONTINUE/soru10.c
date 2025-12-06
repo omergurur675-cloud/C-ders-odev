de794215dd5534660cd192cf518dc028203b3825
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:53:39 +0300

    Update soru10.c

--- CONTINUE/soru10.c
+++ CONTINUE/soru10.c
@@ -1,3 +1,17 @@
+/*
+SORU 10 : Bu program, 0’dan 48’e kadar olan sayılar içinden sadece 7’nin katlarını ekrana yazdırır.
+
+SORU ALGORİTMASI:
+1- Başla
+2- i = 0
+3- 49’a kadar i’yi 1 artırarak tekrar et
+4-     Eğer i, 7’nin katı değilse devam et (atla)
+5-     Değilse i değerini yazdır
+6- Bitir
+
+*/
+
+
 #include<stdio.h>
 
   int main() {
