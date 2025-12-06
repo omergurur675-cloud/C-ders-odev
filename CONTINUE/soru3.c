cbc6bb0fe105cce66925b1b163830d2d6e347ee8
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:56:19 +0300

    Update soru3.c

--- CONTINUE/soru3.c
+++ CONTINUE/soru3.c
@@ -1,9 +1,22 @@
+/*
+SORU 3 : Bu program, 1’den 100’e kadar olan sayılar içinden sadece 5’in katlarını ekrana yazdırır.
+
+SORU ALGORİTMASI:
+1- Başla
+2- i = 1
+3- 100’e kadar i’yi 1 artırarak tekrar et
+4-     Eğer i, 5’in katı değilse devam et (atla)
+5-     Değilse i’yi yazdır
+6- Bitir
+
+*/
+
 #include <stdio.h>
 
 int main() {
     for(int i = 1; i <= 100; i++) {
-        if(i % 5 != 0) continue;   // 5'in katı değilse atla
-        printf("%d ", i);         // sadece 5 ve katları yazılır
+        if(i % 5 != 0) continue;   
+        printf("%d ", i);         
     }
     return 0;
 }
