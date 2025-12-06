d44f4553981dc1eda9af16792cf4561cceef7fad
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 16:01:34 +0300

    Update soru8.c

--- CONTINUE/soru8.c
+++ CONTINUE/soru8.c
@@ -1,3 +1,17 @@
+/*
+SORU 8 : Bu program, 10 ürünün stok miktarını alır ve stok değeri 0 olmayan ürünleri ekrana yazdırır.
+
+SORU ALGORİTMASI:
+1- Başla
+2- i = 1
+3- 10 ürün için tekrar et
+4-     i. ürünün stok miktarını oku
+5-     Eğer stok 0 ise devam et (atla)
+6-     Değilse stok miktarını yazdır
+7- Bitir
+
+*/
+
 #include<stdio.h>
 
   int main() {
