28d13f8df92db7722cac7b848a02e75b24c9fc78
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:58:30 +0300

    Update soru5.c

--- CONTINUE/soru5.c
+++ CONTINUE/soru5.c
@@ -1,20 +1,29 @@
+*/
+SORU 5 : Bu program, 0’dan 50’ye kadar olan sayılardan 7’nin katı olmayanları ekrana yazdırır ve en sonda i’nin son değerini gösterir.
+
+SORU ALGORİTMASI:
+1- Başla
+2- i = 0
+3- 50’ye kadar i’yi 1 artırarak tekrar et
+4-     Eğer i, 7’nin katı ise devam et (atla)
+5-     Değilse i’yi yazdır
+6- Döngü biterse i değerini yazdır
+7- Bitir
+
+*/
+
 #include<stdio.h>
 
   int main() {
 
     int i;
-
     for(i = 0; i <= 50; i++) {
         if( i % 7 == 0){
             continue;
-        }
-    
+        } 
         printf("%d\n",i); 
-    }
-     
-        printf("%d",i);
-  
-
+    }  
+      printf("%d",i);
   
         return 0;
   }
