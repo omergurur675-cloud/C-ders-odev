80f0bfe96c31c558a9b02b3f178f97c449c2c879
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 16:04:19 +0300

    Update soru9.c

--- CONTINUE/soru9.c
+++ CONTINUE/soru9.c
@@ -1,3 +1,16 @@
+/*
+SORU 9 : Bu program, kullanıcıdan 5 kez isim alır ve boş olmayan isimleri ekrana yazdırır.
+
+SORU ALGORİTMASI:
+1- Başla
+2- 5 kez tekrar et
+3-     Kullanıcıdan isim al
+4-     Eğer ismin ilk karakteri boş ise devam et (atla)
+5-     Değilse ismi yazdır
+6- Bitir
+
+*/
+
 #include<stdio.h>
 
   int main() {
