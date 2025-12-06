6bee7c7e36f97b80f5489c074629b301dc280a0a
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:51:44 +0300

    Update soru1.c

--- CONTINUE/soru1.c
+++ CONTINUE/soru1.c
@@ -1,3 +1,17 @@
+/*
+SORU 1 : Bu program, 1’den 20’ye kadar olan sayılar içinden sadece tek sayıların toplamını hesaplar
+SORU ALGORİTMASI:
+1- Başla
+2- toplam = 0
+3- i = 1
+4- 20’ye kadar i’yi 1 artırarak tekrar et
+5-     Eğer i çift ise devam et (atla)
+6-     Değilse toplam = toplam + i
+7- Döngü bitince toplam değerini yazdır
+8- Bitir
+
+*/
+
 #include<stdio.h>
 
   int main() {
