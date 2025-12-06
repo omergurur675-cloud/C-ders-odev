0cac9f531b37f59e6c3f1b4e0060ff6c7c61b72d
omergurur675-cloud <omergurur675@gmail.com>
2025-12-06 15:59:42 +0300

    Update soru6.c

--- CONTINUE/soru6.c
+++ CONTINUE/soru6.c
@@ -1,3 +1,17 @@
+/*
+SORU 6 : Bu program, 0’dan 30’a kadar olan sayılardan sadece 3’ün veya 5’in katı olanları ekrana yazdırır ve en sonda i’nin son değerini gösterir.
+
+SORU ALGORİTMASI:
+1- Başla
+2- i = 0
+3- 30’a kadar i’yi 1 artırarak tekrar et
+4-     Eğer i ne 3’ün katı ne de 5’in katı ise devam et (atla)
+5-     Değilse i’yi yazdır
+6- Döngü bitince i’nin son değerini yazdır
+7- Bitir
+
+*/
+
 #include<stdio.h>
 
   int main() {
